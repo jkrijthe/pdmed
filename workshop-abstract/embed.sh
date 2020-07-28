8b344fe4cf61aeced05082c206c98169048ffbbf
Jesse Krijthe <jkrijthe@gmail.com>
2020-07-28 14:57:51 +0200

    Removes some older files

--- workshop-abstract/embed.sh
+++
@@ -1,6 +0,0 @@
-gs \
-  -sFONTPATH=/Library/Fonts:/System/Library/Fonts:/Users/jkrijthe/Library/Fonts \
-  -o poster_pdmed_embedded.pdf \
-  -sDEVICE=pdfwrite \
-  -dPDFSETTINGS=/prepress \
-   poster_pdmed.pdf
